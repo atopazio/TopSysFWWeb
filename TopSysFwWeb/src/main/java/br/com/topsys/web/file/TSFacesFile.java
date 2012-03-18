db836971c9bd2e0b84c9007849d97f03cc76eec8
andreluismps <andreluismps@gmail.com>
2012-03-18 16:45:03 -0300

    Alterando para projeto Maven e colocando métodos que estavam sendo declarados no projeto TopSysFW que faziam uso de API's WEB

---
+++ TopSysFwWeb/src/main/java/br/com/topsys/web/file/TSFacesFile.java
@@ -0,0 +1,37 @@
+package br.com.topsys.web.file;
+
+import java.sql.Blob;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Essa Classe � usada para manipular arquivos. 
+ * 
+ * @author Andr� Monteiro
+ */
+public final class TSFacesFile {
+	
+	public static void blobToFileTemp(Blob blob, String nomeArquivo) {
+
+		try {
+
+			HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+
+			response.setContentType("application/*");
+
+			response.setHeader("Content-disposition", "attachment; filename=" + nomeArquivo);
+
+			ServletOutputStream out = response.getOutputStream();
+			out.write(blob.getBytes(1, (int) blob.length()));
+			out.flush();
+			FacesContext.getCurrentInstance().responseComplete();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+	}
+
+}
